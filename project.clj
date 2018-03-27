652f0796ea808533e99079eaa3631ff59c7aaa49
Jonathon McKitrick <jonathon.mckitrick@tstllc.net>
2018-03-27 11:46:30 -0400

    Shadow-CLJS build

--- project.clj
+++ project.clj
@@ -7,15 +7,7 @@
                  [cljs-ajax "0.7.3"]
                  [devcards "0.2.4"]
                  [sablono "0.8.3"]
-                 [org.webjars/bootstrap "4.0.0"]
-                 ;; _CLJSJS_
-                 #_[cljsjs/react "16.2.0-3"]
-                 #_[cljsjs/react-dom "16.2.0-3"]]
-
-  ;; _DBLBND_
-  ;; Exclude the dependencies here, but replace them
-  ;; by empty namespaces that mirror the structure.
-  :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]
+                 [org.webjars/bootstrap "4.0.0"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]
             [lein-ancient "0.6.15"]
